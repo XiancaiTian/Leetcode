af5dbbf4e9d10a42a328e7a421c74fc6edb82f4c
helenalee1994 <42988516+helenalee1994@users.noreply.github.com>
2019-03-13 21:49:22 +0800

    better solution

--- q35.py
+++ q35.py
@@ -13,3 +13,20 @@ class Solution(object):
                 return i
             i += 1
         return i
+
+    
+## solution 2: faster than 100.00% , less than 69.35% 
+class Solution(object):
+def searchInsert(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    i = 0
+    for i, v in enumerate(nums):
+        if v == target:
+            return i
+        elif v > target:
+            return i
+    return i+1
