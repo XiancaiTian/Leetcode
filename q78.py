25e61302a110b16d19dc6138ec7cc101686a0d63
helenalee1994 <42988516+helenalee1994@users.noreply.github.com>
2019-06-22 21:38:01 +0800

    Update q78.py

--- q78.py
+++ q78.py
@@ -5,3 +5,19 @@ class Solution:
             for i in range(len(res)):
                 res += [res[i] + [n]]
         return res
+    
+    
+class Solution(object):
+    def subsets(self, nums):
+        self.nums = nums
+        ans = []
+        length = len(nums)
+        for num in range(length+1):
+            ans += self.pick(length, abs(num))
+        return ans
+    
+    def pick(self, n , k):
+        if k == 0:
+            return [[]]
+        else:
+            return [ele + [self.nums[val-1]] for val in range(k,n+1) for ele in self.pick(val-1, k-1)]
