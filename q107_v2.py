fc8f2b5e6917e5b8c1e501c2a76873a5909288c1
Helena Lee <42988516+helenalee1994@users.noreply.github.com>
2020-02-02 13:43:31 +0800

    Create q107_v2.py

---
+++ q107_v2.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        lst = []
+        self.recursion(root, 0, lst)
+        return lst[::-1]
+    
+    def recursion(self, root, level, lst):
+        if root:
+            if len(lst)<level+1:
+                lst.append([root.val])
+            else:
+                lst[level].append(root.val)
+            self.recursion(root.left, level+1, lst)
+            self.recursion(root.right, level+1, lst)
