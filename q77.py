62fee68465129aae823979b8f73f3ad702058d31
Helena Lee <42988516+helenalee1994@users.noreply.github.com>
2020-02-08 20:43:51 +0800

    Update q77.py

--- q77.py
+++ q77.py
@@ -14,22 +14,23 @@ class Solution(object):
 # Helena's solution, based on recursion    
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        
         # recursion soln
         # https://www.youtube.com/watch?v=qzdTZWW1X8A
-        
-        return self.recursion(n+1,k)
-
-    def recursion(self, n, k):
-        '''
-        n: ranges from 1,2,n
-        k: num of selection
-        '''
         ans = []
-        for i in range(1, n):
-            if k-1>0:
-                sub = [lst + [i] for lst in self.recursion(i, k-1)]
-            else:
+        for i in range(1, n+1):
+            if k-1 > 0:
+                sub = [[i] + lst for lst in self.combine(i-1, k-1)]
+            elif k == 1:
                 sub = [[i]]
+            else:
+                sub = []
             ans.extend(sub)
         return ans
+'''
+combine(5,3)
+[1+ combine(0, 2)]  --> None
+[2,+ combine(1, 2)] --> None
+[3,+ combine(2, 2)] -->[3, 2, 2], [3, 2, 1]
+[4,+ combine(3, 2)] -->[4, 1, 2], [4, 1, 3],.....
+[5,+ combine(4, 2)] --> ......
+'''
