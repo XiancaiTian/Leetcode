21390a10db04241cb926e4a995713f73eb2302cd
Helena Lee <42988516+helenalee1994@users.noreply.github.com>
2020-03-05 13:32:18 +0800

    Update q92.py

--- q92.py
+++ q92.py
@@ -1,15 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-
-'''Note: we want a reverse, not an exchange
-I plan to get three parts: (1, 234, 5)
-then reverse (234) then join them back
-'''
-
 class Solution(object):
     def reverseBetween(self, head, m, n):
         
@@ -25,7 +13,6 @@ class Solution(object):
         pre = None
         for _ in range(n - m + 1):
             cur.next, pre, cur = pre, cur, cur.next
-        
-        p.next.next = cur # dummy = [0, 1, 2, 5]; combine part A+C
-        p.next = pre      # dummy = [0, 1, 4, 3, 2, 5]; combine part A+B+C
+        p.next.next = cur # dummy = [0, 1]+?+[5]; combine part A+C
+        p.next = pre      # dummy = [0, 1, 4, 3, 3, 5]; combine part A+B+C
         return dummy.next
