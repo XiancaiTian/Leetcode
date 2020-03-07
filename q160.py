da31d606e40e1f91e825ef670084e8993e018ebe
Helena Lee <42988516+helenalee1994@users.noreply.github.com>
2020-03-07 20:39:33 +0800

    Create q160.py

---
+++ q160.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# refers to two pointers solution
+# https://leetcode.com/problems/intersection-of-two-linked-lists/solution/
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        
+        pa, pb = headA, headB
+        while pa is not pb:          
+            pa = pa.next if pa else headB
+            pb = pb.next if pb else headA
+    
+        return pa
