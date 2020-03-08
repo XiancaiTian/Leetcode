408bf06ff9b9a3fcf4009d156e6c8c86478482d4
Helena Lee <42988516+helenalee1994@users.noreply.github.com>
2020-03-08 14:14:02 +0800

    Update q33.py

--- q33.py
+++ q33.py
@@ -14,32 +14,36 @@ class Solution(object):
         else:
             index = self.find_value(nums[:pivot], target, 0)
         return index
-        
-    def find_value(self, nums, target, pivot):
+    
+    
+    # template: may find nothing
+    def find_value(self, nums, target, base):
         left, right = 0, len(nums)-1
         while left<=right:
             mid = (left + right) //2
             if nums[mid] == target:
-                return pivot + mid
+                return base + mid
             else:
                 if target > nums[mid] :
-                    left = left + 1
+                    left = mid + 1
                 else:
-                    right = right -1
+                    right = mid -1
         return -1
         
-    
+    # template: must find something
     def find_pivot(self, nums, target):
-        left, right = 0, len(nums)-1
-        while left<right:
+        left, right = 0, len(nums)-1 
+        if nums[left] < nums[right]:
+            return 0
+        
+        while left<=right:
             mid = (left + right) //2
             if nums[mid] > nums[mid+1]:
                 return mid+1
             else:
-                if nums[left] < nums[mid]:
-                    left = left + 1
-                if nums[right] > nums[mid]:
-                    right = right -1
-        return 0
-
+                if nums[mid] < nums[left]:
+                    right = mid -1
+                else:
+                    left = mid + 1
+        
         
