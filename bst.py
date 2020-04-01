d8e1d66d90135d6ebaaa853c942515266b4d8ef0
Helena Lee <42988516+helenalee1994@users.noreply.github.com>
2020-04-01 15:39:01 -0700

    Update bst.py

--- bst.py
+++ bst.py
@@ -27,27 +27,81 @@ class Solution(object):
                 
 # LEETCODE Q35      
 class Solution(object):
-def searchInsert(self, nums, target):
-    """
-    :type nums: List[int]
-    :type target: int
-    :rtype: int
-    """
-    left = 0
-    right = len(nums)-1
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums)-1
 
-    while left<=right:
-        mid = (left + right)//2 
-        if nums[mid] == target:
-            return mid
-        if nums[mid] < target:
-            left = mid + 1
-        elif nums[mid] > target:
-            right = mid -1
+        while left<=right:
+            mid = (left + right)//2 
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid -1
 
-    return left
+        return left
 
-                
- 
-                
-                
+# LEETCODE Q33               
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        n = len(nums)
+        if n == 0:
+            return -1
+        if n == 1:
+            return 0 if nums[0] == target else -1 
+
+        pivot = self.find_pivot(nums, target)
+        if target <= nums[-1]:
+            index = self.find_value(nums[pivot:], target, pivot)
+        else:
+            index = self.find_value(nums[:pivot], target, 0)
+        return index
+
+
+    # template: may find nothing
+    def find_value(self, nums, target, base):
+        left, right = 0, len(nums)-1
+        while left<=right:
+            mid = (left + right) //2
+            if nums[mid] == target:
+                return base + mid
+            else:
+                if target > nums[mid] :
+                    left = mid + 1
+                else:
+                    right = mid -1
+        return -1
+
+    # template: must find something
+    def find_pivot(self, nums, target):
+        left, right = 0, len(nums)-1 
+        if nums[left] < nums[right]:
+            return 0
+
+        while left<=right:
+            mid = (left + right) //2
+            print(left, right, mid)
+            if nums[mid] > nums[mid+1]:
+                return mid+1
+            else:
+                if nums[mid] < nums[left]:
+                    right = mid -1
+                else:
+                    left = mid + 1
+
+
+
+
+
+
