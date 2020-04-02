d084a6a4879244067aa69ed6f49a6e9e11031a10
Helena Lee <42988516+helenalee1994@users.noreply.github.com>
2020-04-01 17:34:49 -0700

    Update bst.py

--- bst.py
+++ bst.py
@@ -108,7 +108,35 @@ class Solution(object):
                     left = mid + 1
 
 
-
-
+# LEETCODE Q81
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        
+        n = len(nums)
+        left, right = 0, n-1
+        while left <= right:
+            mid = left + (right - left) // 2
+            print(left, right, mid)
+            if nums[mid]==target:
+                return True
+            if nums[mid]<nums[right]:
+                if (nums[mid] < target and nums[right] >= target):
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            elif nums[mid]>nums[right]:
+                if (nums[mid] > target and nums[left] <= target):
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                right -= 1
+            
+        return False
 
 
