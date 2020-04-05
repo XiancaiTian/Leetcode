40b9372b665f26a278b50712fdb5dbc21d20b254
Helena Lee <42988516+helenalee1994@users.noreply.github.com>
2020-04-05 12:10:02 -0700

    Update q200.py

--- q200.py
+++ q200.py
@@ -23,3 +23,55 @@ class Solution(object):
         self.dfs(grid, i-1, j)
         self.dfs(grid, i, j+1)
         self.dfs(grid, i, j-1)
+        
+# BFS solution
+# utilize iteration
+# have a queue
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if grid == []:
+            return 0 
+        
+        n = len(grid)
+        m = len(grid[0])
+        
+        visited = [[0] * m for i in range (n)]
+        
+        def bfs(i,j):
+            visited[i][j]=1
+            queue = [(i,j)]
+            while queue :
+                # print(queue)
+                point = queue.pop(0)
+                i = point[0]
+                j = point[1]
+                if i > 0 and visited[i-1][j] == 0 and grid[i-1][j] == "1" :
+                    queue.append((i-1, j ))
+                    visited[i-1][j]=1
+                    
+                if j > 0 and visited[i][j-1] == 0 and grid[i][j-1] == "1" :
+                    queue.append((i, j-1 ))
+                    visited[i][j-1]=1
+                    
+                if i < n-1 and visited[i+1][j] == 0 and grid[i+1][j] == "1" :
+                    queue.append((i+1, j ))
+                    visited[i+1][j]=1
+                    
+                if j < m-1 and visited[i][j+1] == 0 and grid[i][j+1] == "1" :
+                    queue.append((i, j+1 ))
+                    visited[i][j+1]=1
+                
+            return
+        
+        count  =0 
+        # bfs(0,0)
+        # print(visited)
+        
+        for  i in range (n):
+            for j in range(m):
+                # print(visited)
+                if grid[i][j] == "1" and  visited[i][j]==0 :
+                    count +=1
+                    bfs(i,j)
+        
+        return count 
