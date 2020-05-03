7805bdacd81bb91aa441b0b8d19b5e8a32de5038
Helena Lee <42988516+helenalee1994@users.noreply.github.com>
2020-05-02 19:46:00 -0700

    Update q1231.py

--- q1231.py
+++ q1231.py
@@ -2,17 +2,17 @@ class Solution:
     """
     very similar to leetcode 410
     1, need to change line 12 from total = n to total = n
-    2, need to change make n_splits equal to K+ 1, not K\
+    2, need to change make n_splits equal to K+ 1, not K
     3, need to start from min(sweetness) , in line 30
     """ 
     def maximizeSweetness(self, sweetness: List[int], K: int) -> int:
         def canCut(sweetness, mid, n_splits= K+1):
             total, count = 0, 1
             """
-            # leetcode 410 的目標是每一塊都不要比mid大
+            # leetcode 410 的目標是每一塊都不要比mid大，或是等於mid
             mid = 9, [5,6,7,8,9,1,2,3,4] ---> [[5], [6], [7], [8], [9], [1,2,3],[4]]
             
-            # leetcode 1231 的目標是每一塊都要比mid大
+            # leetcode 1231 的目標是每一塊都要比mid大，或是等於mid
             mid = 9, [5,6,7,8,9,1,2,3,4] ---> [[5,6], [7,8], [9], [1,2,3,4]]
             """
             for n in sweetness:
