cb0aa994625b4d1d6559107970980a9f2d7d6d3a
Helena Lee <42988516+helenalee1994@users.noreply.github.com>
2020-05-02 21:43:00 -0700

    Update q69.py

--- q69.py
+++ q69.py
@@ -1,3 +1,29 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        # Edge cases...
+        if x == 0:
+            return 0
+        if x == 1:
+            return 1
+        
+        # mid的平方會大於input x 嗎?
+        def higerThan(mid, x):
+            return mid**2 > x
+        
+        # 套用lower bound binary search template
+        # 找到第一個平方大於input的數
+        # 如果input = 8, 1*1 -->F, 2*2 -->F, 3*3 -->T, 則binary search會return 3
+        # 因為題目要求只想看integer部分，所以最後答案應該是3-1=2
+        l, r = 0, x
+        while l < r:
+            mid = l + (r-l)//2
+            if higerThan(mid,x):
+                r = mid
+            else:
+                l = mid + 1
+        return l-1
+
+
 class Solution(object):
     def mySqrt(self, x):
         """
