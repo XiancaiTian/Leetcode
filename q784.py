bbcd35d65d77c99bbf3097166deeeee6c15789bb
Helena Lee <42988516+helenalee1994@users.noreply.github.com>
2020-05-05 16:59:38 -0700

    Update q784.py

--- q784.py
+++ q784.py
@@ -1,3 +1,4 @@
+# My solution
 class Solution:
     """
     "a1b2" can be simplified as [ab, aB, Ab, AB] plus the unchanged number 1 2 
@@ -20,3 +21,29 @@ class Solution:
         ans = []
         dfs(S, 0, len(S), 0, [], ans)
         return ans
+
+    
+"""
+Author: Huahua
+Running time: 92 ms
+"""
+class Solution:
+  def letterCasePermutation(self, S):
+    ans = []
+ 
+    def dfs(S, i, n):
+      if i == n:
+        ans.append(''.join(S))
+        return
+    
+      # 無論是字母或數字，都讓深度+1, 進到下一個dfs()
+      dfs(S, i + 1, n)      
+      if not S[i].isalpha(): return
+      # 如果是字母的話，要做大小寫轉換，然後多一個dfs()
+      S[i] = chr(ord(S[i]) ^ (1<<5))
+      dfs(S, i + 1, n)
+      # 大小寫轉換用完了，把他復原
+      S[i] = chr(ord(S[i]) ^ (1<<5))
+    
+    dfs(list(S), 0, len(S))
+    return ans
