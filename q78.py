53c15184e2f1d7426418fec70b1558208f3c77a8
Helena Lee <42988516+helenalee1994@users.noreply.github.com>
2020-05-04 23:05:26 -0700

    Update q78.py

--- q78.py
+++ q78.py
@@ -1,4 +1,26 @@
 class Solution:
+    # 套用花花模板
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+        # dfs function 跟Leetcode 77 完全一樣
+        # d 代表深度，k 代表取幾個數(固定), s 代表從第幾個數字開始取
+        # nums 代表可以選的數字範圍
+        def dfs(nums, d, k, s, curr, ans):
+            if d == k:
+                ans.append(curr[:])
+                return
+            for i in range(s, len(nums)):
+                curr.append(nums[i])
+                dfs(nums, d+1, k, i+1, curr, ans)
+                curr.pop()
+                
+        ans = []
+        for k in range(0, len(nums)+1):
+            dfs(nums, 0, k, 0, [], ans)
+        return ans
+
+
+class Solution:
     def subsets(self, nums: [int]) -> [[int]]:
         res = [[]]
         for n in nums:
