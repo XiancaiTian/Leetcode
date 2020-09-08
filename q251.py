d818ca10d518fd0dc549a00c7a28763ae916631b
Helena Lee <42988516+helenalee1994@users.noreply.github.com>
2020-09-07 22:55:19 -0700

    Update q251.py

--- q251.py
+++ q251.py
@@ -28,3 +28,33 @@ class Vector2D(object):
         :rtype: bool
         """
         return self.latest != None
+
+    
+   # ==================
+
+class Vector2D:
+
+    def __init__(self, v: List[List[int]]):
+        
+        def val():
+            for inner in v:
+                for element in inner:
+                    yield element
+            
+        self.val = val()
+        self.peek = None
+
+    def next(self) -> int:
+        if self.hasNext():
+            ans = self.peek
+            self.peek = None
+            return ans
+
+    def hasNext(self) -> bool:
+        if self.peek is not None:
+            return True
+        try:
+            self.peek = next(self.val)
+            return True
+        except:
+            return False
