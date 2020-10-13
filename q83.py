6f290732ef7dc04871c7164367fb6611c54c52cd
Helena Lee <42988516+leehelenah@users.noreply.github.com>
2020-10-12 17:59:25 -0700

    Update q83.py

--- q83.py
+++ q83.py
@@ -22,3 +22,19 @@ class Solution(object):
                 hold = head_ptr.next.next
                 head_ptr.next = hold
         return dummy.next
+
+# More elegant solution
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        ptr = head
+        while ptr and ptr.next:
+            if ptr.val == ptr.next.val:
+                ptr.next = ptr.next.next
+            else:
+                ptr = ptr.next
+        return head
