48310fff4137e6fba132dc2c14ed3ca29fc4419d
Helena Lee <42988516+leehelenah@users.noreply.github.com>
2020-10-17 23:53:55 -0700

    Update q41.py

--- q41.py
+++ q41.py
@@ -1,3 +1,4 @@
+# I don't remember why I have this solution lol...
 class Solution(object):
     def firstMissingPositive(self, nums):
         """
@@ -14,3 +15,31 @@ class Solution(object):
                 i+=1
         return i
                 
+# It's easy if you follow the solution of educative
+# Edge cases are tricky: [] needs to return 1 and [1] needs to return 2
+
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        maxi_nums = len(nums)
+        i = 0
+        while i < maxi_nums:
+            loc = nums[i] - 1
+            while loc >= 0 and loc < maxi_nums and nums[i] != (i+1) and nums[i] != nums[loc]:
+                a = nums[loc]
+                nums[loc] = nums[i]
+                nums[i] = a
+                loc = nums[i] - 1
+            i += 1
+        
+        i = 0
+        while i < maxi_nums:
+            if nums[i]!= (i+1):
+                return (i+1)
+            i += 1
+            
+        return maxi_nums + 1 
