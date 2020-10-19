3b408f9972a422ea324e347ff31f9c0dc5b3bd53
Helena Lee <42988516+leehelenah@users.noreply.github.com>
2020-10-19 13:31:51 -0700

    Create BFS-template.py

---
+++ BFS-template.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+class Solution(object):
+    def levelOrder(self, root):
+        result = []
+        if root is None:
+            return result
+        
+        queue = deque()
+        queue.append(root)
+        while queue:
+            levelSize = len(queue)
+            currentLevel = []
+            for _ in range(levelSize):
+                currentNode = queue.popleft()
+                # add the node to the current level
+                currentLevel.append(currentNode.val)
+                # insert the children of current node in the queue
+                if currentNode.left:
+                    queue.append(currentNode.left)
+                if currentNode.right:
+                    queue.append(currentNode.right)
+
+            result.append(currentLevel)
+        
+        return result
