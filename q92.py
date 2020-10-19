379ec67f43e8768fe7c271245b02582a4f8206cb
Helena Lee <42988516+leehelenah@users.noreply.github.com>
2020-10-18 17:27:35 -0700

    Update q92.py

--- q92.py
+++ q92.py
@@ -1,18 +1,26 @@
 class Solution(object):
     def reverseBetween(self, head, m, n):
-        
         if m == n:
             return head
+        
+        # part A, let dummy be 0 -> 1
         p = dummy = ListNode(0)
         dummy.next = head
         for _ in range(m - 1):
             p = p.next
-        # dummy = [0, 1] ; part A
         
-        cur = p.next
-        pre = None
+        # part B 
+        curr = p.next # 2
+        prev = None
         for _ in range(n - m + 1):
-            cur.next, pre, cur = pre, cur, cur.next
-        p.next.next = cur # dummy = [0, 1]+?+[5]; combine part A+C
-        p.next = pre      # dummy = [0, 1, 4, 3, 3, 5]; combine part A+B+C
+            holder = curr.next
+            curr.next = prev
+            prev = curr
+            curr = holder
+            
+        # combine part A+C
+        p.next.next = curr # let dummy be 0 -> 1 -> ? -> 5
+        # combine part A+B+C
+        p.next = prev
+        
         return dummy.next
