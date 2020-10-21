ee817202ecbbb646f972a053cc8bd99b4a3ed673
Helena Lee <42988516+leehelenah@users.noreply.github.com>
2020-10-21 13:54:32 -0700

    Update DFS-recursive-template.py

--- DFS-recursive-template.py
+++ DFS-recursive-template.py
@@ -25,12 +25,6 @@ class Solution(object):
     
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-# Definition for a binary tree node.
-# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
