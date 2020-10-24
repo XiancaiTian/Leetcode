0d9a1e6ecb05bacdcdc709b79876709e573d113e
Helena Lee <42988516+leehelenah@users.noreply.github.com>
2020-10-24 13:56:19 -0700

    Update q112.py

--- q112.py
+++ q112.py
@@ -18,3 +18,34 @@ class Solution:
                 if not root.left and not root.right:
                     if path == sum: return True
         return False
+
+    
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        self.target_sum = sum
+        self.ans = False
+        self.pathSum(root, 0)
+        return self.ans
+    
+    def pathSum(self, root, path_sum):
+    
+        if root is None:
+            return
+        
+        path_sum += root.val
+        
+        if root.left is None and root.right is None:
+            if path_sum == self.target_sum:
+                self.ans = True
+        else:
+            self.pathSum(root.left, path_sum)
+            self.pathSum(root.right, path_sum)
+        
+        path_sum -= root.val
+                        
