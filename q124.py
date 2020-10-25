4c48d159a3add02a8ffff8e47991657fde3d51f3
Helena Lee <42988516+leehelenah@users.noreply.github.com>
2020-10-24 23:53:10 -0700

    Update q124.py

--- q124.py
+++ q124.py
@@ -1,96 +1,45 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
-class Solution(object):
-    def maxPathSum(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """    
-        maxi = root.val
-        self.max = None # the true answer
-        summ = self.readtree(root)
-        return self.max
-    
-    def readtree(self, root):
-        if not root:
-            return 0
-        # eveything got val, plus one
-        # add downstream depth
-        summ_left = self.readtree(root.left)
-        summ_right = self.readtree(root.right)
-        
-        # this question is ask about path, not sum of triangle
-        if root.val:
-            summ_left = max(0, summ_left)
-            summ_right = max(0, summ_right)
-        temp = root.val + summ_left + summ_right
-        self.max = max(temp, self.max)
-        summ = root.val + max(summ_left, summ_right)
-        return summ
 
-'''''solution2'''''
-class Solution(object):
-    def maxPathSum(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        
-        '''
-        trick 1: not always pick left or right
-        sometimes need to have left and right
-        
-        trick 2: input may be negative value
-        
-        should refer to 
-        https://github.com/XiancaiTian/Leetcode/blob/master/q124.py
-        That solution looks better
-        Basic idea: if left>0, bring it. if right>0 bring it.
-        
-        """ 
-        # if only one node
-        if not root.left and not root.right:
-            return root.val
-        
-        # pick parent only
-        pathsum = [root.val]
-        l = self.recursion(root.left, pathsum)
-        r = self.recursion(root.right, pathsum)
-        
-        # pick first root and a single children
-        pathsum.append(root.val + max(l,r))
-        # pick first root and both children
-        pathsum.append(root.val + l + r )
-        # pick a single children
-        if root.left:
-            pathsum.append(l)
-        if root.right:
-            pathsum.append(r)
+# should consider every node as a new start
+
+# should return the better path in each recursion
+# Note: there is no need to reach the leaf
+
+# better path is selected from 
+# (self + left, self + right, self only, self + left + right)
+
+# return path is selected from 
+# (self + left, self + right, self only)
+
+# edge case, what is the tree looks like [-5]
+
+class Solution:
+    def maxPathSum(self, root: TreeNode) -> int:
+        self.maxi_sum = root.val
+        self.dfs(root)
+        return self.maxi_sum
+
+    def dfs(self, root):
+        if root:
+            left_path = self.dfs(root.left)
+            right_path = self.dfs(root.right)
             
-        return max(pathsum)
-    
+            three_options = max(root.val, 
+                              root.val + left_path, 
+                              root.val + right_path)
+            
+            self.maxi_sum = max(self.maxi_sum, 
+                                three_options, 
+                                root.val + left_path + right_path)
+            return three_options
         
-    def recursion(self, root, pathsum):
-        if not root: 
-            return 0
         else:
-            l = self.recursion(root.left, pathsum)
-            r = self.recursion(root.right, pathsum)
-            print(root.val, l, r)
-            # pick parent and left or right children
-            value = root.val + max(l,r)
-            # or parenet only
-            value = max(root.val, value)
-            pathsum.append(value)
+            return 0
             
-            # pick parent and both chidren
-            pathsum.append(l + r + root.val)
-            
-            # pick parent only
-            pathsum.append(root.val)
-            
-            return value
+
